eb0ee01761f9e682526cf0a5046806825e50a478
Harshit Behl <harshitbehl671@gmail.com>
2025-03-19 12:50:46 +0530

    minor  update

--- main/src/components/customComponents/mainComponents/ProductCard.tsx
+++ main/src/components/customComponents/mainComponents/ProductCard.tsx
@@ -10,8 +10,7 @@ import "swiper/css/pagination";
 
 const ProductCard = ({ data }: { data: IProduct }) => {
   const images = data?.images  ? data?.images?.split(",") : [];
-
-  
+  console.log(images);
   return (
     <CustomCard>
       <Link href={`/products/details?product=${data?.name}&product_id=${data?.id}`} className="block">
